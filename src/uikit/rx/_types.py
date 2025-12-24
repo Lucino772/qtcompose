073efa5819271d3fb4b08bc041f9ae4d946ff089
Lucino772 <lucapalmi772@gmail.com>
2025-12-24 10:43:02 +0100

    feat: made uikit.rx protocols runtime checkable

--- src/uikit/rx/_types.py
+++ src/uikit/rx/_types.py
@@ -1,17 +1,19 @@
 from __future__ import annotations
 
 from collections.abc import Callable
-from typing import Protocol, TypeAlias, TypeVar
+from typing import Protocol, TypeAlias, TypeVar, runtime_checkable
 
 T_co = TypeVar("T_co", covariant=True)
 U_contra = TypeVar("U_contra", contravariant=True)
 T = TypeVar("T")
 
 
+@runtime_checkable
 class Disposable(Protocol):
     def dispose(self) -> None: ...
 
 
+@runtime_checkable
 class Observable(Protocol[T_co]):
     def apply(
         self, func: Callable[[Observable[T_co]], Observable[U_contra]]
@@ -24,6 +26,7 @@ class Observable(Protocol[T_co]):
     def subscribe(self, func: Callable[[T_co], None]) -> Disposable: ...
 
 
+@runtime_checkable
 class Observer(Protocol[U_contra]):
     def push(self, val: U_contra) -> None: ...
 
